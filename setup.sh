5dc8803859c4c96f5a8cdce24e2bb92c43a9a039
Ludvik Sørensen <ludviksoerensen@gmail.com>
2022-03-25 11:16:12 +0100

    Bug fixes

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ echo "${COLOUR_YELLOW}Configuring Raspberrry Pi...\e[0m"
 read -p 'Hostname: ' hostname
 
 echo "${COLOUR_GREEN}The hostname has been changed to ${HOSTNAME}\e[0m"
-hostnamectl $hostname
+hostname $hostname
 
 # Enabling SSH
 systemctl enable sshd
@@ -21,12 +21,12 @@ sudo ufw allow from 128.39.112.1/22 to any port 22 proto tcp
 echo "${COLOUR_GREEN}Student IP-range has been whitelisted.\e[0m"
 
 # Change URL idk
-sed -i 's/Kongsberg/Vestfold/' "/etc/xdg/openbox/autostart"
+sudo sed "s/Kongsberg/Vestfold/g" "/etc/xdg/openbox/autostart"
 echo "${COLOUR_GREEN}Kiosk URL updated.\e[0m"
 
 echo "${COLOUR_YELLOW}Raspberrry Pi has been configured!\e[0m"
 
 # Reboot
 echo "${COLOUR_YELLOW}Reboot required, rebooting in 10 seconds.\e[0m"
-#sleep 10
-#sudo reboot
+sleep 10
+sudo reboot
