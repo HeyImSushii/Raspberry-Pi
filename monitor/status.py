af5391fd287e59f94f49d60f8b970672f1de4e61
Ludvik Sørensen <ludviksoerensen@gmail.com>
2022-04-07 13:30:51 +0200

    Update status.py

--- monitor/status.py
+++ monitor/status.py
@@ -8,8 +8,15 @@ cpu_usage = psutil.cpu_percent()
 cpu_temp = CPUTemperature.temperature
 memory_usage = psutil.virtual_memory().percent
 
+def sec_to_hours(seconds):
+    a=str(seconds//3600)
+    b=str((seconds%3600)//60)
+    c=str((seconds%3600)%60)
+    d=["{} hours {} mins {} seconds".format(a, b, c)]
+    return d
+
 print(hostname)
-print(uptime%3600/60)
+print(sec_to_hours(uptime))
 print(str(cpu_usage) + "%")
 print(str(cpu_temp) + "*C")
 print(str(memory_usage) + "%")
