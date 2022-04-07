562e6723f375f297380e65995d2b33e955fe11fe
Ludvik Sørensen <ludviksoerensen@gmail.com>
2022-04-07 13:51:35 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,7 +2,7 @@
 
 hostname=$(hostname);
 kernel=$(uname -r);
-uptime=$(uptime -p | sed s/" minutes"// | sed s/"up "//);
+uptime=$(uptime -p | sed s/","// | sed s/"up "//);
 cpu_usage=$(echo ""$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%" | sed s/"CPU Usage: "//);
 
 jo hostname=$hostname kernel=$kernel uptime=$uptime cpu_usage=$cpu_usage > hostname.json
