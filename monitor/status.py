18ce935be496f5ef6442310b83c5f3b165afd523
Ludvik Sørensen <ludviksoerensen@gmail.com>
2022-04-07 13:26:02 +0200

    Update status.py

--- monitor/status.py
+++ monitor/status.py
@@ -2,18 +2,16 @@ from dis import dis
 import psutil
 import platform
 from gpiozero import CPUTemperature
-import time
+from uptime import uptime
+import datetime
 
 hostname = platform.uname().node
-uptime = time.time() - psutil.boot_time() / 60
 cpu_usage = psutil.cpu_percent()
 cpu_temp = CPUTemperature.temperature
 memory_usage = psutil.virtual_memory().percent
-#disk_usage = psutil.disk_usage().percent
 
 print(hostname)
-print(str(uptime) + " minutes")
+print(str(datetime.timedelta(seconds=uptime)))
 print(str(cpu_usage) + "%")
 print(str(cpu_temp) + "*C")
 print(str(memory_usage) + "%")
-#print(str(disk_usage) + "%")
